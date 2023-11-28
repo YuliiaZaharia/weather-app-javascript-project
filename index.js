615bcc01ba9b13b2087539f2db6ac5687d54f685
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-28 12:34:16 +0300

    cteated variables to show temp, description, city etc in wether app

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ const input = document.querySelector("#input");
 input.addEventListener("keypress", enter);
 
 function enter(e) {
-if (e.keycode === 13) {
+if (e.keyCode === 13) {
     getInfo(input.value);
 }
 }
 
-async function getInfo(data) {
-const result = await fetch(`${api.root}weather?q=${data}&units=metric&aAPPID=${api.key}`);
+async function getInfo (data) {
+const result = await fetch(`${api.root}weather?q=${data}&units=metric&appID=${api.key}`);
 const resultWeather = await result.json();
+console.log(resultWeather)
 displayResultWeather(resultWeather);
 }
 
 function displayResultWeather(resultWeather) {
     let city = document.querySelector("#city");
-    city.textContent = `${result.Weather.name}`
+    city.textContent = `${resultWeather.name}, ${resultWeather.sys.country}`; 
+
+    let temperature = document.querySelector("#temp");
+    temperature.innerHTML = `${Math.round(resultWeather.main.temp)}<span>°</span>`;
+
+    let feelsLike = document.querySelector("#feels");
+    feelsLike.innerHTML = `${Math.round(resultWeather.main.feels_like)}<span>°</span>`;
+
+    let description = document.querySelector("#description");
+    description.innerHTML = `${resultWeather.weather[0].description}`;
+
+    let variation = document.querySelector("#variation");
+    variation.innerHTML = "Min: " + `${Math.round(resultWeather.main.temp_min)}<span>°</span>` + " " + "Max " + `${Math.round(resultWeather.main.temp_max)}<span>°</span>`
 }
