a18558419cdd10b78f5823283fb17e18012db79d
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-28 10:44:59 +0300

    new function getInfo()

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ input.addEventListener("keydown", enter);
 
 function enter(e) {
 if (e.keycode === 13) {
-    getInfo();
+    getInfo(input.value);
 }
+}
+
+async function getInfo(data) {
+const result = await fetch(`${api.root}weather?q=${data}&units=metric&appID=${api.key}`);
 }
